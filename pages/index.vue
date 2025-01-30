43905dcb97f4215b5782f55bd5dc62728d78eae8
Denis Yashin <denis.yashin@enkisoft.ru>
2025-01-30 21:58:45 +0300

    get from store

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
     <h1>Страница index</h1>
+    <h2>sdf {{ store.smth }}</h2>
   </div>
 </template>
+<script setup lang="ts">
+import { useCocktailStore } from '~/store/use-cocktail.store';
+
+const store = useCocktailStore();
+
+</script>
--- store/use-cocktail.store.ts
+++ store/use-cocktail.store.ts
@@ -1,7 +1,9 @@
-import { defineStore } from "pinia";
+import { createPinia, defineStore, setActivePinia } from "pinia";
+
 import { availableCocktails } from "~/consts/availableCocktails.const";
 
 export const useCocktailStore = defineStore("cocktail", () => {
-  
+  const smth = ref(5);
 
+  return { smth };
 });
