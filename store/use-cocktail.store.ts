02c11aabe631b18d27dad49589cdcc5537306ad7
Denis Yashin <denis.yashin@enkisoft.ru>
2025-01-30 22:23:25 +0300

    prepare request

--- store/use-cocktail.store.ts
+++ store/use-cocktail.store.ts
@@ -1,9 +1,27 @@
-import { createPinia, defineStore, setActivePinia } from "pinia";
+import { defineStore } from "pinia";
+import { from, Subject, switchMap } from "rxjs";
 
 import { availableCocktails } from "~/consts/availableCocktails.const";
 
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=<cocktail_code>';
+
 export const useCocktailStore = defineStore("cocktail", () => {
   const smth = ref(5);
+  const currentCode: Ref<string> = ref('');
+  const setSubject$ = new Subject();
 
-  return { smth };
+  const set = (newCode: string) => {
+    setSubject$.next(newCode);
+  }
+
+  setSubject$.pipe(switchMap((newCode) => from($fetch(url, {
+      query: {
+        s: newCode
+      }
+    }))
+  )).subscribe(a => {
+    console.log(a);
+  });
+
+  return { smth, set };
 });
