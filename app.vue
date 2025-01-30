6cab2a841ebd39512a90243e976ed131c896f70c
Denis Yashin <denis.yashin@enkisoft.ru>
2025-01-30 16:30:06 +0300

    some

--- app.vue
+++ app.vue
@@ -1,6 +1,30 @@
+<script setup lang="ts">
+import { NuxtPage } from '#components';
+
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <header>Коктейли</header>
+  <div class="layout">
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/posts/1">Post 1</NuxtLink></li>
+        <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
+      </ul>
+    </nav>
+    <NuxtPage class="layout__content"/>
   </div>
 </template>
+
+<style>
+.layout {
+  display: flex;
+}
+
+.layout__content {
+  background-color: red;
+  flex-grow: 1;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Страница index</h1>
+    <app-alert>Это автоимпортируемый компоненте</app-alert>
+  </div>
+</template>
