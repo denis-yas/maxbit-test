03c5a19cc3c1f5b72d0dcdc446289234c33e552c
Denis Yashin <denis.yashin@enkisoft.ru>
2025-01-30 21:17:30 +0300

    store

---
+++ store/use-cocktail.store.ts
@@ -0,0 +1,7 @@
+import { defineStore } from "pinia";
+import { availableCocktails } from "~/consts/availableCocktails.const";
+
+export const useCocktailStore = defineStore("cocktail", () => {
+  
+
+});
