fde105acc241c918c62830d5e27dd972a22ce420
Denis Yashin <denis.yashin@enkisoft.ru>
2025-01-31 16:44:28 +0300

    redirect to first item

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
+import { availableCocktails } from './consts/availableCocktails.const';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@nuxt/eslint"],
+  modules: ['@pinia/nuxt', '@nuxt/eslint'],
   build: {
-    transpile: ["rxjs"],
+    transpile: ['rxjs'],
+  },
+  routeRules: {
+    '/': { redirect: '/' + availableCocktails[0] },
   },
   // app: {
   //   pageTransition: { name: 'page', mode: 'out-in' }
