d1f54163f5a8f768d197cb708dae82a2d57dfebb
Denis Yashin <denis.yashin@enkisoft.ru>
2025-02-02 13:15:08 +0300

    disable dev tools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { availableCocktails } from './consts/availableCocktails.const';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ['@pinia/nuxt', '@nuxt/eslint', '@nuxtjs/tailwindcss'],
   build: {
     transpile: ['rxjs'],
